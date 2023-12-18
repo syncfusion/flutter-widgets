344c212310a75591f2cc9675fa1a88ee6a1df19f
Yuvaraj-Gajaraj <yuvaraj.gajaraj@syncfusion.com>
2023-12-18 15:34:22 +0530

    Removed unwanted file changes

--- packages/syncfusion_pdfviewer_macos/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/syncfusion_pdfviewer_macos/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
-import syncfusion_pdfviewer_macos
-import url_launcher_macos
+import syncfusion_flutter_pdfviewer_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
