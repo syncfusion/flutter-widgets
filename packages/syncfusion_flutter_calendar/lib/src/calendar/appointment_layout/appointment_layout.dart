fb64e00cff91e5cfe90fd9e5ef744a780415bea6
Anishishi <aki.nishi.work@gmail.com>
2024-01-19 23:15:53 +0900

    fix

--- packages/syncfusion_flutter_calendar/lib/src/calendar/appointment_layout/appointment_layout.dart
+++ packages/syncfusion_flutter_calendar/lib/src/calendar/appointment_layout/appointment_layout.dart
@@ -1837,6 +1837,7 @@ class _AppointmentRenderObject extends CustomCalendarRenderObject {
     double? radius;
 
     final List<int> keys = monthAppointmentCountViews.keys.toList();
+    print(keys.length);
     for (int i = 0; i < keys.length; i++) {
       final int index = keys[i];
       final RRect moreRegionRect = monthAppointmentCountViews[index]!;
