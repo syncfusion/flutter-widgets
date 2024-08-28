e6b535c4c364516bcaf513cf29d36a832a2e96f5
Otrek <89265952+fingineering@users.noreply.github.com>
2024-08-28 13:23:28 +0200

    Update cartesian_chart.dart

--- packages/syncfusion_flutter_charts/lib/src/charts/cartesian_chart.dart
+++ packages/syncfusion_flutter_charts/lib/src/charts/cartesian_chart.dart
@@ -1364,12 +1364,7 @@ class SfCartesianChartState extends State<SfCartesianChart>
       if (renderObject != null &&
           renderObject.attached &&
           renderObject is RenderConstrainedLayoutBuilder) {
-        try {
-          // try to use the old code
-          renderObject.markNeedsBuild();
-        } catch (e) {
           renderObject.markNeedsPaint();
-        }
       }
     }
   }
