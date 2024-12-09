5af88c0432d2e5a89581c83beb029b124f570c4d
fenil-gol-oceanmtech <fenilgol5@gmail.com>
2024-12-09 11:29:29 +0530

    version update
    version update

---
+++ packages/syncfusion_flutter_pdfviewer/example/build/url_launcher_android/generated/source/buildConfig/release/io/flutter/plugins/urllauncher/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.flutter.plugins.urllauncher;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "io.flutter.plugins.urllauncher";
+  public static final String BUILD_TYPE = "release";
+}
